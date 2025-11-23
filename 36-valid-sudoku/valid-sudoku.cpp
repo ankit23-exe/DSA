c931d878fcf90c80ea38b0ed4efa81d183639177
Ankit <23it034@jssaten.ac.in>
2025-11-23 15:42:15 +0530

    Time: 12 ms (9.68%) | Memory: 25.9 MB (9.08%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
+        /*
         // row checking
         for (int i = 0; i < 9; i++) {
             unordered_set<char> s;
@@ -44,6 +45,31 @@ public:
                         s.insert(board[i][j]);
                     }
                 }
+            }
+        }
+        return true;
+
+        */
+         unordered_set<string> s;
+        for (int i = 0; i < 9; i++) {
+           
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.'){
+                    continue;
+                }
+                string rowKey = "row" + to_string(i) + "_" + to_string(board[i][j]);
+                string colKey = "col" + to_string(j) + "_" + to_string(board[i][j]);
+                string boxKey = "box" + to_string(i / 3) + "_" +
+                                to_string(j / 3) + "_" + to_string(board[i][j]);
+
+                if (s.count(rowKey) || s.count(colKey) || s.count(boxKey)) {
+                    // duplicate found
+                    return false;
+                }
+
+                s.insert(rowKey);
+                s.insert(colKey);
+                s.insert(boxKey);
             }
         }
         return true;
