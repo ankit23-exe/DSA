265c3762b53bc1fed5821ec667580b97931b9163
Ankit <23it034@jssaten.ac.in>
2025-12-01 13:38:55 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (69.74%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -10,40 +10,29 @@
  */
 class Solution {
 public:
-    ListNode* createLinkedList(vector<int>& v) {
-        if (v.empty())
-            return nullptr;
-
-        ListNode* head = new ListNode(v[0]); // first element
-        ListNode* temp = head;
-
-        for (int i = 1; i < v.size(); i++) {
-            temp->next = new ListNode(v[i]); // create next node
-            temp = temp->next;               // move forward
-        }
-
-        return head;
-    }
-
     ListNode* reverseKGroup(ListNode* head, int k) {
-        vector<int> v;
-        ListNode* h = head;
-        ListNode* t = head;
-
-        while (h != nullptr) {
-            v.push_back(h->val);
-            h = h->next;
-        }
-        for (int i = 0; i < v.size() - (v.size() % k); i += k) {
-            int start = i;
-            int end = i + k - 1;
-            while (start < end) {
-                swap(v[start], v[end]);
-                start++;
-                end--;
-            }
+        
+        ListNode* temp = head;
+        int count=0;
+        while(count<k){
+            if(temp==nullptr) return head;
+            temp=temp->next;
+            count++;
         }
 
-         return createLinkedList(v);
+        ListNode* prenode =reverseKGroup(temp,k);
+
+        temp =head;
+        count=0;
+        while(count<k){
+            ListNode* nxt = temp->next;
+            temp->next=prenode;
+            prenode=temp;
+            temp=nxt;
+            count++;
+
+        }
+        return prenode;
+       
     }
 };
