4beaec16c0bd270faadd25e91f8d3d57512513f2
Ankit <23it034@jssaten.ac.in>
2025-12-06 18:41:54 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (60.84%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
+        int num1= NULL;
+        int count1=0;
+        int num2= NULL;
+        int count2=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==num1){
+                count1++;
+            }else if(nums[i]==num2){
+                count2++;
+            }else if(count1==0){
+                num1=nums[i];
+                count1=1;
+
+            }else if(count2==0){
+                num2=nums[i];
+                count2=1;
+
+            }else{
+                count1--;count2--;
+            }
+        }
+        //verify krna hoga
+        count1=0;
+        count2=0;
+        for(int i=0;i<nums.size();i++){
+            if(num1==nums[i]){
+                count1++;
+            }else if(num2==nums[i]){
+                count2++;
+
+            }
+
+        }
         vector<int> ans;
-        int n = nums.size();
-        for (int num : nums) {
-            if (mp.find(num) != mp.end()) {
-                mp[num]++;
-            } else {
-                mp[num] = 1;
-            }
-        }
-        for (auto item : mp) {
-            if (item.second > (n / 3)) {
-                ans.push_back(item.first);
-            }
-        }
+
+        if(count1>(nums.size()/3)) ans.push_back(num1);
+
+        if(count2>(nums.size()/3)) ans.push_back(num2);
+
         return ans;
-    };
+    }
 };
