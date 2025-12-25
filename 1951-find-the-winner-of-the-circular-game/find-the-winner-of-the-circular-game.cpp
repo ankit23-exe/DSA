7533618753233b7326f86b3555f133b8161c4b5e
Ankit <23it034@jssaten.ac.in>
2025-12-25 19:13:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (94.62%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
-    int findTheWinner(int n, int k) {   //joseph recurrence
-        int winner = 0; 
-
-        for (int i = 2; i <= n; i++) {
-            winner = (winner + k) % i;
+    int winner(int n, int k){
+        if(n==1){
+            return 0;
         }
 
-        return winner + 1; 
+        int idx=winner(n-1,k);
+        idx=(idx+k)%n;
+        return idx;
+        
+        }
+
+    int findTheWinner(int n, int k) {   //joseph recurrence
+        
+        return winner(n,k)+ 1; 
     }
 };
