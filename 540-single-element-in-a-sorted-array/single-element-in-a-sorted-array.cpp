e7a74a27fc5c9cbcdae14e6a439b908d0deb303f
Ankit <23it034@jssaten.ac.in>
2025-12-27 22:50:43 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.19%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            // make mid even
+            if (mid % 2 == 1) mid--;
+
+            if (nums[mid] == nums[mid + 1]) {
+                // single element is on the right
+                low = mid + 2;
+            } else {
+                // single element is on the left (or mid itself)
+                high = mid;
+            }
+        }
+
+        return nums[low];
+    }
+};
