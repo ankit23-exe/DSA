db49f9cfdb39f6a211a0f4437b0196d2068c60fd
Ankit <23it034@jssaten.ac.in>
2026-01-09 12:33:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.66%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     vector<int> v;
-    int f(int n){
-         if(n==0) return 0;
-        if(n==1) return 1;
-
-        if(v[n]!=-1){
-            return v[n];
-        }
-        return v[n] = fib(n-1)+fib(n-2);
-         
-    }
-    
-
     int fib(int n) {
-        v.resize(31,-1);
-        return f(n);
+        if(n<=1) return n;
+        v.resize(n+1,-1);
+        v[0]=0;
+        v[1]=1;
+        for(int i=2;i<=n;i++){
+            v[i]=v[i-1]+v[i-2];
+        }
+        return v[n];
     }
        
 };
