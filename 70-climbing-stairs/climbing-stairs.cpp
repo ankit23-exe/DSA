4074480844cb0f4b1065047eee7a96b7d9046d93
Ankit <23it034@jssaten.ac.in>
2026-01-17 12:39:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (70.05%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int arr[n+1];
+        
         if(n<3){
             return n;
         }
-        arr[0]=0;
-        arr[1]=1;
-        arr[2]=2;
+       
+        int a=1;
+        int b=2;
 
-        for(int i =3;i<=n;i++){   //bottom up approach hai ya
-            arr[i]=arr[i-1]+arr[i-2];
+        for(int i =3;i<=n;i++){   //bottom up approach hai
+            int c =b+a;
+            a=b;
+            b=c;
         }
-        return arr[n];
+        return b;
     }
 };
