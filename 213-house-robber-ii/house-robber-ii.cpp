fc9cfe7669dcf4001dc9a8b6e8d22549087ad069
Ankit <23it034@jssaten.ac.in>
2026-01-21 12:20:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (38.59%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    vector<int> t;
-    int solve(int i ,vector<int>& nums){
-        if(i>=nums.size()){
-            return 0;
-        } 
-        //memorisation use kr rha hai
-        if(t[i]!=-1){
-            return t[i];
-        }
-
-        //steal
-        int steal = nums[i] +solve(i+2,nums);
-        //skip
-        int skip = solve(i+1,nums);
-
-        return t[i] = max(steal,skip);
-    }
     int rob(vector<int>& nums) {
-
-        if(nums.size()==1){
+        int n = nums.size();
+        if(n==1){
             return nums[0];
         }
-        if(nums.size()==2){
-            return max(nums[0],nums[1]);
+        vector<int> t(n+1,0);
+        //case 1 , house 1 ko select krenga and last house ko select nhi krenga
+        for(int i = 1;i<=n-1;i++){
+            int skip = t[i-1];
+
+            int take = nums[i-1] + (i-2>=0?t[i-2]:0);
+
+            t[i]=max(skip,take);
         }
-        t.resize(nums.size() ,-1);
-        //1 house mai chori krenga
-        int lastno = nums.back();
-        nums.pop_back(); 
-        int ans1 = solve(0,nums);
+        int result1= t[n-1];
 
-        //1st house mai nhi krenga 
-        nums.push_back(lastno);
-        t.assign(nums.size(),-1);
-        int ans2= solve(1,nums);
+        t.clear();
 
-        return max (ans1, ans2);
+        //case 2 , house 1 ko nhi lenga
+        t[0]=0;
+        t[1]=0;
+
+        for(int i =2;i<=n;i++){
+            int skip = t[i-1];
+
+            int take = nums[i-1] + (i-2>=0?t[i-2]:0);
+
+            t[i]=max(skip,take);
+        }
+        int result2= t[n];
+
+        return max(result1,result2);
     }
 };
