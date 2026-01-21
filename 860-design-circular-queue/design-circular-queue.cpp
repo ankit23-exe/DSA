e2c9745247d85c8eef63776c86eeea70bb0c4a74
Ankit <23it034@jssaten.ac.in>
2026-01-21 22:29:33 +0530

    Time: 2 ms (68.80%) | Memory: 23.7 MB (8.05%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.cpp
+++ 860-design-circular-queue/design-circular-queue.cpp
@@ -1,82 +1,74 @@
 class MyCircularQueue {
 public:
     vector<int> q;
-    int front=-1;
-    int rear=-1;
-    int size;
-    MyCircularQueue(int k) {
-        q.resize(k,0);
-        size=k;
-        
+    int front = -1;
+    int rear = -1;
+    int k = 0;
+    MyCircularQueue(int s) {
+        q.resize(s, 0);
+        k = s;
     }
-    
-    bool enQueue(int value) {
-        if(front==rear && front ==-1){
-            q[0]=value;
-            front=0;
-            rear=0;
-            return true;
-        }else if((rear+1)%size==front){
-            return false;
-        }else{
-            int idx= (rear+1)%size;
-            q[idx]=value;
-            rear=(rear+1)%size;
-            return true;
-        }
-        
-    }
-    
-    bool deQueue() {
-        if(front==rear && front ==-1){
-            return false;
-        }else if(front==rear && front!=-1){
-            front =-1;
-            rear=-1;
-            return true;
-            
-        }
-        else{
 
-            front=(front+1)%size;
+    bool enQueue(int value) {
+        if (front == rear && front == -1) { // no element alreadt
+            front = 0;
+            rear = 0;
+            q[front] = value;
+            
+            return true;
+        } else if ((rear + 1) % k == front) {
+            return false;
+        } else {
+            q[(rear + 1) % k] = value;
+            rear = (rear+1)%k;
             return true;
         }
-        
     }
-    
+
+    bool deQueue() {
+        if (front == rear && front == -1) {
+            return false;
+        } else if (front == rear) {
+            front = -1;
+            rear = -1;
+            return true;
+        } else {
+            front = (front + 1) % k;
+            return true;
+        }
+    }
+
     int Front() {
-        if(front==-1){
+        if (front == rear && front == -1) {
             return -1;
-        }else{
+        } else {
             return q[front];
         }
-        
     }
-    
+
     int Rear() {
-        if(rear==-1){
+        if (front == rear && front == -1) {
             return -1;
-        }else{
+        } else {
             return q[rear];
         }
-        
     }
-    
+
     bool isEmpty() {
-        if(front==rear && front ==-1){
+        if (front == rear && front == -1) {
             return true;
+        } else {
+            return false;
         }
-        return false;
-        
-        
     }
-    
+
     bool isFull() {
-        if((rear+1)%size==front){
+        if ((rear + 1) % k == front) {
             return true;
+
+        } else {
+            return false;
         }
-        return false;
-        
     }
 };
 
