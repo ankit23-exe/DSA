30d2f9db9b6d4c2c80d2e5543d29972c89e03b35
Ankit <23it034@jssaten.ac.in>
2026-02-07 10:07:53 +0530

    Time: 43 ms (27.96%) | Memory: 26.4 MB (44.82%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -4,19 +4,11 @@ public:
         stack<char> st;
         int count =0;
         for (int i = 0; i < s.size(); i++) {
-            char c = s[i];
-            if(st.empty()){
-                st.push(c);
+            if(!st.empty() && s[i]=='a' && st.top()=='b'){
+                st.pop();
+                count++;
             }else{
-                if(c == st.top()){
-                    st.push(c);
-                }else if(c=='b'&& st.top()=='a'){
-                    st.push(c);
-                }else if(c=='a' && st.top()=='b'){
-                    count++;
-                    st.pop();
-                   
-                }
+                st.push(s[i]);
             }
         }
 
