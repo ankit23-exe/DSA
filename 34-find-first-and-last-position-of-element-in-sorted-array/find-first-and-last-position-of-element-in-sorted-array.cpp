89f767a7618f0776f8957e98865e8d275d5f94dd
Ankit <23it034@jssaten.ac.in>
2026-02-08 23:41:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (18.66%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
-    int find(vector<int>& nums, int target, int n,bool flag){
-        int ans=-1;
-        int l = 0;
-        int r= n-1;
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(nums[mid]==target){
-                ans = mid;
-                if(flag){ //going right side
-                    l = mid+1;
-                }else{
-                    r = mid-1;
-                }
-
-            }else if(nums[mid]>target){
-                r = mid-1;
-            }else{
-                l = mid+1;
-            }
-        }
-        return ans;
-    }
+   
     vector<int> searchRange(vector<int>& nums, int target) {
         int n= nums.size();
-        int left = find(nums,target,n,false);
-        int right = find(nums,target,n,true);
-
-        return {left,right};
+        int lb = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int ub = upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+        if(lb ==ub) return {-1,-1};
+        else{
+            return {lb,ub-1};
+        }
+        
     }
 };
