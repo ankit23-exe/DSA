8b4611c7926448b0403abd4bc2b99b8aa536f7fa
Ankit <23it034@jssaten.ac.in>
2026-02-21 20:45:31 +0530

    Time: 12 ms (51.22%) | Memory: 35.2 MB (19.45%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    bool possible(int wt,vector<int>& weights,int days){
-        int n = weights.size();
-        int cursize =0;
-        int needday=0;
-        for(int i=0;i<n;i++){
-            if(cursize + weights[i]<= wt){
-                cursize+=weights[i];
+    bool possible(int wt,vector<int>& weights, int days){
+        int day=0;
+        int cursize=0;
+        for(int i=0;i<weights.size();i++){
+            if(cursize+weights[i] >wt){
+                    day++;
+                    cursize=weights[i];
             }else{
-                cursize=weights[i];
-                needday++;
+                cursize+=weights[i];
             }
         }
-        if(cursize!=0) needday+=1;
-
-        if (needday <= days) return true;
-        return false;
+        if(cursize != 0) day+=1;
+        if(day<=days) return true;
+        else return false;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-         int l = *max_element(weights.begin(), weights.end());
-        int r = accumulate(weights.begin(), weights.end(), 0);
+        int n = weights.size();
+        int l =0;
+        int r = 0;
+        for(int i=0;i<n;i++){
+            l = max(weights[i],l);
+            r+=weights[i];
+        }  
 
         while(l<=r){
-            int mid = l + (r-l)/2;
+            int mid = l +(r-l)/2;
             if(possible(mid,weights,days)){
-                r=mid-1;
+                r= mid-1;
             }else{
                 l = mid+1;
             }
