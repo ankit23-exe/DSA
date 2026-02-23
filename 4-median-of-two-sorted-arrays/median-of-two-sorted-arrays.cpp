90dfa275e6f404be8cff2b30883f590325a664be
Ankit <23it034@jssaten.ac.in>
2026-02-23 17:46:57 +0530

    Time: 3 ms (29.68%) | Memory: 95 MB (80.51%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,13 +1,70 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        for(int i=0;i<nums2.size();i++){
-            nums1.push_back(nums2[i]);
-        }
-        sort(nums1.begin(),nums1.end());
+        int totalsize = nums1.size() + nums2.size();
+        int m = nums1.size();
+        int n = nums2.size();
 
-        int n = nums1.size();
-        if(n%2==1) return nums1[n/2];
-        else return 1LL*(nums1[n/2] + nums1[(n/2)-1])/2.0;
+        int ele1 = -1;
+        int ele2 = -1;
+        int i = 0;
+        int j = 0;
+
+        int k = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                if (k == totalsize / 2 - 1) {
+                    ele1 = nums1[i];
+                }
+                if (k == totalsize / 2) {
+                    ele2 = nums1[i];
+                    
+                }
+
+                i++;
+
+            } else {
+                if (k == totalsize / 2 - 1) {
+                    ele1 = nums2[j];
+                }
+                if (k == totalsize / 2) {
+                    ele2 = nums2[j];
+                    
+                }
+
+                j++;
+            }
+            k++;
+        }
+
+        while (i < m) {
+            if (k == totalsize / 2 - 1) {
+                ele1 = nums1[i];
+            }
+            if (k == totalsize / 2) {
+                ele2 = nums1[i];
+                
+            }
+
+            i++;
+            k++;
+        }
+
+        while (j < n) {
+            if (k == totalsize / 2 - 1) {
+                ele1 = nums2[j];
+            }
+            if (k == totalsize / 2) {
+                ele2 = nums2[j];
+                
+            }
+
+            j++;
+            k++;
+        }
+
+        if(totalsize%2==1) return ele2;
+        else return (ele1+ele2)/2.0;
     }
 };
