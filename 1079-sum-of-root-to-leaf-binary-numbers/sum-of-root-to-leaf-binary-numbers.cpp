127fb1e45ac19db85963a0a5d59f0e449a82e30b
Ankit <23it034@jssaten.ac.in>
2026-02-24 14:53:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (34.02%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
@@ -1,47 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<string> ans;
-    void traversal(string temp,TreeNode* root){
 
-        if (!root) return;
-        temp.push_back(root->val + '0');  
+    int dfs(TreeNode* root, int curr) {
+        if (!root) return 0;
 
-        if (!root->left && !root->right) {
-            ans.push_back(temp);
-            return;
-        }
-        traversal(temp,root->left);
-        traversal(temp,root->right);
+        
+        curr = curr * 2 + root->val;
 
+       
+        if (!root->left && !root->right)
+            return curr;
+
+        
+        return dfs(root->left, curr) + dfs(root->right, curr);
     }
-    int sumRootToLeaf(TreeNode* root) {
-        string temp= "";
-        traversal(temp,root);
-        int finalans =0;
-        for(auto &digi:ans){
-            int n = digi.size();
-            int k =0;
-            int num =0;
-            for(int i=n-1;i>=0;i--){
-                if(digi[i]=='1'){
-                    num +=pow(2,k);
-                }
-                k++;
-            }
-            finalans +=num;
-        }
 
-        return finalans;
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root, 0);
     }
 };
