a14cdb18dfac382cd04cbf898f6396dc18d0f68c
Ankit <23it034@jssaten.ac.in>
2026-03-14 19:44:47 +0530

    Time: 43 ms (16.40%) | Memory: 26.9 MB (10.34%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void make(int n,int k, char prev,string &temp,vector<string> &all_strings){
+        if(temp.size()==n){
+            all_strings.push_back(temp);
+            return;
+        }
+        for(int i=0;i<=2;i++){
+            if(prev != 'a'+i){
+                temp.push_back('a'+i);
+                make(n,k,'a'+i,temp,all_strings);
+                temp.pop_back();
+            }
+           
+        }
+    }
+    string getHappyString(int n, int k) {
+        vector<string> all_strings;
+        string temp = "";
+        make(n,k,'x',temp,all_strings);
+
+        sort(all_strings.begin(),all_strings.end());
+        if(k>all_strings.size()){
+            return "";
+        }
+        return all_strings[k-1];
+    }
+};
