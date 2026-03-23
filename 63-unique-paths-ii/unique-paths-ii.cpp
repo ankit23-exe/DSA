f59c5e8f9db8674aaaae0d7ca702a64427330ae2
Ankit <23it034@jssaten.ac.in>
2026-03-23 11:58:14 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (99.85%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     int t[101][101];
-    int solve(int x,int y,vector<vector<int>>& grid){
+    int solve(int x, int y, vector<vector<int>>& grid) {
 
-        if(x>=grid.size() || y>=grid[0].size() || grid[x][y]==1){
+        if (x >= grid.size() || y >= grid[0].size() || grid[x][y] == 1) {
             return 0;
         }
 
-        if(x==grid.size()-1 && y ==grid[0].size()-1){
+        if (x == grid.size() - 1 && y == grid[0].size() - 1) {
             return 1;
         }
-        if(t[x][y]!= -1){
+        if (t[x][y] != -1) {
             return t[x][y];
         }
-        
-        int right = 0;
-        if(y+1< grid[0].size() && grid[x][y+1] ==0 ){
-            right  = solve(x,y+1,grid);
-        }
-        int down = 0;
-        if(x+1<grid.size() && grid[x+1][y] !=1){
-            down = solve(x+1,y,grid);
-        }
+
+        int right = solve(x, y + 1, grid);
+
+        int down = solve(x + 1, y, grid);
 
         return t[x][y] = right + down;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        memset(t,-1,sizeof(t));
-        return solve(0,0,grid);
+        memset(t, -1, sizeof(t));
+        return solve(0, 0, grid);
     }
 };
-
