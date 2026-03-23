ca787cb45b5d72782981aa15cfe5ea992a7b9012
Ankit <23it034@jssaten.ac.in>
2026-03-23 15:56:47 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (40.12%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int t[201][201];
-    int solve(int x,int y,vector<vector<int>>& grid){
+    int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-
-        if(x == m-1 && y == n-1){
-            return grid[m-1][n-1];
-        }
-
-        if(x>=m || y>=n) return INT_MAX;
-
-        if(t[x][y] !=-1){
-            return t[x][y];
-        }
+        vector<vector<int>> dgrid(m,vector<int>(n,0));
+        dgrid[0][0]=grid[0][0];
         
-        int right = solve(x,y+1,grid);
-        int down = solve(x+1,y,grid);
+        for(int i=1;i<m;i++){
+            dgrid[i][0] = grid[i][0] + dgrid[i-1][0];
+        }
+        for(int j = 1;j<n;j++){
+            dgrid[0][j]= grid[0][j] + dgrid[0][j-1]; 
+        }
 
-        return t[x][y] = grid[x][y] + min(right,down);
-    }
-    int minPathSum(vector<vector<int>>& grid) {
-        memset(t,-1,sizeof(t));
-        return solve(0,0,grid);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dgrid[i][j] = grid[i][j] + min(dgrid[i-1][j],dgrid[i][j-1]);
+            }
+        }
+
+        return dgrid[m-1][n-1];
     }
 };
