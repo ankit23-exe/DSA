44ae357256334d807eb6e67115d4bbd5c9135797
Ankit <23it034@jssaten.ac.in>
2026-03-30 11:40:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (24.36%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-       
-        vector<char> a1 = {s1[0], s1[2]};
-        vector<char> b1 = {s2[0], s2[2]};
-        
-        
-        vector<char> a2 = {s1[1], s1[3]};
-        vector<char> b2 = {s2[1], s2[3]};
-        
-        sort(a1.begin(), a1.end());
-        sort(b1.begin(), b1.end());
-        sort(a2.begin(), a2.end());
-        sort(b2.begin(), b2.end());
-        
-        return a1 == b1 && a2 == b2;
+        int even[26] = {0};
+        int odd[26] = {0};
+
+        for(int i=0;i<4;i++){
+            if(i==0 || i== 2){
+                even[s1[i]-'a']++;
+                even[s2[i]-'a']--;
+            }else{
+                odd[s1[i]-'a']++;
+                odd[s2[i]-'a']--;
+            }
+        }
+        for(int i=0;i<26;i++){
+            if(even[i]!=0 || odd[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
