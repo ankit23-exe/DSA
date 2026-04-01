30128dc496cfd85a302c4c06bf6345c3d3cf247d
Ankit <23it034@jssaten.ac.in>
2026-04-01 22:14:23 +0530

    Time: 53 ms (73.25%) | Memory: 194.7 MB (66.47%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n = positions.size();
+        
+    
+        vector<int> idx(n);
+        for(int i = 0; i < n; i++) idx[i] = i;
+        
+        sort(idx.begin(), idx.end(), [&](int a, int b){
+            return positions[a] < positions[b];
+        });
+        
+        stack<int> st; 
+        
+        for(int id : idx) {
+            if(directions[id] == 'R') {
+                st.push(id);
+            } else {
+               
+                while(!st.empty() && healths[id] > 0) {
+                    int top = st.top();
+                    
+                    if(healths[top] < healths[id]) {
+                        st.pop();
+                        healths[id]--; 
+                        healths[top] = 0;
+                    }
+                    else if(healths[top] > healths[id]) {
+                        healths[top]--; 
+                        healths[id] = 0;
+                        break;
+                    }
+                    else {
+                        
+                        healths[top] = 0;
+                        healths[id] = 0;
+                        st.pop();
+                        break;
+                    }
+                }
+            }
+        }
+        
+       
+        vector<int> res;
+        for(int i = 0; i < n; i++) {
+            if(healths[i] > 0) {
+                res.push_back(healths[i]);
+            }
+        }
+        
+        return res;
+    }
+};
