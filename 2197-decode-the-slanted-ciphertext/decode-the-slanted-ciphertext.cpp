d45703b8890977b6ec44b45b543a7e8945be3ff7
Ankit <23it034@jssaten.ac.in>
2026-04-04 23:01:12 +0530

    Time: 41 ms (40.68%) | Memory: 42.7 MB (55.93%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.cpp
@@ -3,29 +3,16 @@ public:
     string decodeCiphertext(string encodedText, int rows) {
         
         int n = encodedText.size();
-        if(rows ==1 || n==0 ) return encodedText;
+        if(rows == 1 || n==0 ) return encodedText;
         int col = n/rows;
-        vector<vector<char>> grid(rows,vector<char>(col,' '));
-        //making the grid
-        int k =0;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<col;j++){
-                grid[i][j] = encodedText[k++];
-            }
-        }
-        // traversing and making string
         string ans ="";
-        for(int i=0;i<col;i++){
-            int c = i;
-            int r= 0;
-            while(c<col && r<rows){
-                ans+=grid[r][c];
-                c++;r++;
+        for(int c=0;c<col;c++){
+            for(int j=c;j<n;j+=col+1){
+                ans+=encodedText[j];
             }
-
+            
         }
-        // poping the trail spaces
-        int i =ans.size()-1;
+        int i=ans.size()-1;
         while(ans[i]==' '){
             ans.pop_back();
             i--;
