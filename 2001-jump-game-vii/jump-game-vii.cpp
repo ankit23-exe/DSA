a44cdb57d6ca67b077441e9e36c7fcd21af24e3f
Ankit <23it034@jssaten.ac.in>
2026-05-25 18:35:25 +0530

    Time: 18 ms (19.79%) | Memory: 28.9 MB (29.77%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+        vector<int>t(n,0);
+
+        t[0]=1;
+        int count = 0;
+        for(int j=1;j<=n-1;j++){
+            if(j-minJump >=0){
+                count+=t[j-minJump];
+            }
+            if(j-maxJump-1 >=0){
+                count -=t[j-maxJump-1];
+            }
+
+            if(s[j]=='0' && count >0){
+                t[j]=1;
+            }
+        }
+
+        return t[n-1]>0;
+    }
+};
