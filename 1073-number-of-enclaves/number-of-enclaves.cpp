30205e42321178044c5c02cdb21d23a31d98397d
Ankit <23it034@jssaten.ac.in>
2026-05-29 20:21:41 +0530

    Time: 342 ms (5.20%) | Memory: 65.3 MB (5.04%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,53 +1,43 @@
 class Solution {
 public:
-    void dfs(int i, int j,
-             unordered_map<string, vector<pair<int, int>>>& adjlist,
-             set<pair<int, int>>& visited) {
-        visited.insert({i, j});
-        string key = to_string(i) + "+" + to_string(j);
-
-        for (auto neigh : adjlist[key]) {
-            if (!visited.count(neigh)) {
-                dfs(neigh.first, neigh.second, adjlist, visited);
+    int m;
+    int n;
+    vector<vector<int>>direction = {{0,-1},{-1,0},{0,1},{1,0}};
+    void dfs(int i, int j,vector<vector<int>>& grid) {
+        grid[i][j]=0;
+        
+        for (auto d : direction) {
+            int newi = i+d[0];
+            int newj = j+d[1];
+            if (newi>=0 && newi<m && newj>=0 && newj<n) {
+                if(grid[newi][newj]== 1)
+                    dfs(newi,newj,grid);
             }
         }
     }
     int numEnclaves(vector<vector<int>>& grid) {
-        unordered_map<string, vector<pair<int, int>>> adjlist;
-        int oneCount = 0;
-        int m = grid.size();
-        int n = grid[0].size();
+        
+        m = grid.size();
+        n = grid[0].size();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    string key = to_string(i) + "+" + to_string(j);
-                    oneCount++;
-                    if (j - 1 >= 0 && grid[i][j - 1] == 1) {
-                        adjlist[key].push_back({i, j - 1});
-                    }
-                    if (j + 1 < n && grid[i][j + 1] == 1) {
-                        adjlist[key].push_back({i, j + 1});
-                    }
-                    if (i - 1 >= 0 && grid[i - 1][j] == 1) {
-                        adjlist[key].push_back({i - 1, j});
-                    }
-                    if (i + 1 < m && grid[i + 1][j] == 1) {
-                        adjlist[key].push_back({i + 1, j});
-                    }
+                bool isBoundary = (i==0 || i==m-1 ||j==0 || j==n-1);
+                if(isBoundary && grid[i][j]==1){
+                    dfs(i,j,grid);
                 }
+                
             }
         }
-        set<pair<int, int>> visited;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                bool isBoundary =
-                    (i == 0 || i == m - 1 || j == 0 || j == n - 1);
 
-                if (isBoundary && grid[i][j] == 1) {
-                    dfs(i, j, adjlist, visited);
-                }
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(grid[i][j]==1) ans++;
             }
         }
-        return oneCount - visited.size();
+        return ans;
+
+
+       
     }
 };
