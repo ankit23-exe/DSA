c2d3761b1bfad61c20b4dd3cb6939112c000ddbb
Ankit <23it034@jssaten.ac.in>
2026-05-30 15:47:46 +0530

    Time: 20 ms (43.35%) | Memory: 24.2 MB (67.60%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -11,39 +11,71 @@ public:
         {1, 1}    // Bottom-Right
     };
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
+
         int n = grid.size();
-        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+            return -1;
         int count = 1;
-        vector<vector<int>> visited(n, vector<int>(n, INT_MAX));
-        visited[0][0] = 1;
-        priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>>
-            pq;
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        // visited[0][0] = 1;
+        queue<pair<int, int>> que;
+        que.push({0, 0});
+        visited[0][0] = true;
+        // priority_queue<pair<int, pair<int, int>>,
+        //                vector<pair<int, pair<int, int>>>,
+        //                greater<pair<int, pair<int, int>>>>
+        //     pq;
 
-        pq.push({1, {0, 0}});
-        while (!pq.empty()) {
-            auto p = pq.top();
-            pq.pop();
-            int wt = p.first;
-            int i = p.second.first;
-            int j = p.second.second;
+        // pq.push({1, {0, 0}});
+        // while (!pq.empty()) {
+        //     auto p = pq.top();
+        //     pq.pop();
+        //     int wt = p.first;
+        //     int i = p.second.first;
+        //     int j = p.second.second;
 
-            for (auto& d : directions) {
-                int newi = i + d[0];
-                int newj = j + d[1];
-                if (newi < 0 || newi >= n || newj < 0 || newj >= n)
-                    continue;
-                if (grid[newi][newj] == 0) {
-                    if (wt + 1 < visited[newi][newj]) {
-                        visited[newi][newj] = wt + 1;
-                        pq.push({wt + 1, {newi, newj}});
+        //     for (auto &d : directions) {
+        //         int newi = i + d[0];
+        //         int newj = j + d[1];
+        //         if (newi < 0 || newi >= n || newj < 0 || newj >= n)
+        //             continue;
+        //         if (grid[newi][newj] == 0) {
+        //             if (wt + 1 < visited[newi][newj]) {
+        //                 visited[newi][newj] = wt + 1;
+        //                 pq.push({wt + 1, {newi, newj}});
+        //             }
+        //         }
+        //     }
+        // }
+        while (!que.empty()) {
+            int size = que.size();
+            while (size--) {
+                auto p = que.front();
+                que.pop();
+                int i = p.first;
+                int j = p.second;
+                if (i == n - 1 && j == n - 1)
+                    return count;
+                for (auto& d : directions) {
+                    int newi = i + d[0];
+                    int newj = j + d[1];
+
+                    if (newi < 0 || newi >= n || newj < 0 || newj >= n)
+                        continue;
+
+                    if (grid[newi][newj] == 0) {
+                        if (!visited[newi][newj]) {
+                            que.push({newi, newj});
+                            visited[newi][newj] = true;
+                        }
                     }
                 }
             }
+            count++;
         }
-        if(visited[n-1][n-1]==INT_MAX) return -1;
-        return visited[n - 1][n - 1];
+
+        return -1;
+        // if(visited[n-1][n-1]==INT_MAX) return -1;
+        // return visited[n - 1][n - 1];
     }
 };
