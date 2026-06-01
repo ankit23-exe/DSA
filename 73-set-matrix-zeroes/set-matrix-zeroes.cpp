956f9a23339f53cb7c56894c8e8f35078a3c90c2
Ankit <23it034@jssaten.ac.in>
2026-06-01 12:01:45 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (49.45%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,45 +3,51 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        bool firstRowZero = false;
-        bool firstColZero = false;
 
-        // Check first row and col separately
-        for(int i = 0; i < m; i++) {
-            if(matrix[i][0] == 0) 
-                firstColZero = true;
+        int rowImpact = false;
+        int colImpact = false;
+        // checking first row
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                rowImpact = true;
+                break;
+            }
         }
-
-        for(int j = 0; j < n; j++) {
-            if(matrix[0][j] == 0) 
-                firstRowZero = true;
+        // checking first col
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                colImpact = true;
+                break;
+            }
         }
-
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                if(matrix[i][j] == 0) {
+        // now marking the row and col if 0 present in center
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
 
-
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+        // making it zero
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
-
-        if(firstRowZero) {
-            for(int j = 0; j < n; j++) 
-                matrix[0][j] = 0;
+        // last make row 0 if impacted
+        if (rowImpact) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
         }
-        if(firstColZero) {
-            for(int i = 0; i < m; i++) 
+        if (colImpact) {
+            for (int i = 0; i < m; i++) {
                 matrix[i][0] = 0;
+            }
         }
     }
 };
