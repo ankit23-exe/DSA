f0372c2bc92ebbdfcae7a9754d959c9b054cecb1
Ankit <23it034@jssaten.ac.in>
2026-06-01 11:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (32.19%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,52 +1,42 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int flag=0;
-        int n=nums.size();
-        for(int i=n-1;i>0;i-- ){
-            if(nums[i-1]<nums[i]){
-                flag=1;
-            }
-        }
-        if(flag==0){
-            int i=0,j=n-1;
-            while(i<j){
-                swap(nums[i],nums[j]);
-                i++;
-                j--;
-            }
-             return ;
-        }
-       
-
-        int pid=-1;
-        
-        for(int i=n-1;i>0;i--){
-            if(nums[i-1]<nums[i]){
-                pid=i-1;
-                 break;
-            }
-           
-        }
-        int j=n-1;
-
-        while(j>=0){
-            if(nums[j]>nums[pid]){
+        //checking its already in desc order
+        int n = nums.size();
+        bool flag = true;
+        int num = nums[0];
+        for(int i=1;i<n;i++){
+            if(num<nums[i]){
+                flag = false;
                 break;
             }
-            j--;
+            num = nums[i];
         }
-
-        swap(nums[pid],nums[j]);
-
-        pid++;
-
-        j=n-1;
-        while(pid<j){
-            swap(nums[pid],nums[j]);
-            pid++;
+        if(flag){
+            sort(nums.begin(),nums.end());
+            return;
+        }
+        int i;
+        for(i=n-1;i>=0;i--){
+            if(nums[i-1]<nums[i]){
+                break;
+            }
+        }
+        //swap(nums[i],nums[i-1]);
+        for(int j=n-1;j>=0;j--){
+            if(nums[j]>nums[i-1]){
+                swap(nums[j],nums[i-1]);
+                break;
+            }
+        }
+        
+        int j = n-1;
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
             j--;
         }
         return;
+
     }
 };
