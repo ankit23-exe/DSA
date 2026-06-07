a049490b6f5b13a0c1af3820b6f630c5c76a7dfc
Ankit <23it034@jssaten.ac.in>
2026-06-07 10:47:57 +0530

    Time: 186 ms (69.66%) | Memory: 279.1 MB (74.07%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -6,40 +6,40 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_map<int,TreeNode*> mp;
+        unordered_map<int, TreeNode*> mp;
         unordered_set<int> childs;
 
-        for(auto &d:descriptions){
+        for (auto& d : descriptions) {
             int p_val = d[0];
             int c_val = d[1];
 
             TreeNode* p = nullptr;
             TreeNode* c = nullptr;
 
-            p=mp.count(p_val)?mp[p_val]:  new TreeNode(p_val);
-            c=mp.count(c_val)?mp[c_val]:  new TreeNode(c_val);
+            p = mp.count(p_val) ? mp[p_val] : new TreeNode(p_val);
+            c = mp.count(c_val) ? mp[c_val] : new TreeNode(c_val);
 
-            mp[p_val]=p;
-            mp[c_val]=c;
+            mp[p_val] = p;
+            mp[c_val] = c;
 
-            if(d[2]){
-                mp[p_val]->left = mp[c_val];
-            }else{
-                mp[p_val]->right = mp[c_val];
+            if (d[2]) {
+                p->left = c;
+            } else {
+                p->right = c;
             }
-            
 
             childs.insert(c_val);
         }
 
-        for(auto &p:mp){
-            if(!childs.count(p.first)){
+        for (auto& p : mp) {
+            if (!childs.count(p.first)) {
                 return p.second;
             }
         }
