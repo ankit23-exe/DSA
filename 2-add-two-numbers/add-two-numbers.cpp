f90813d4292e609443743a0a409164b038544bba
Ankit <23it034@jssaten.ac.in>
2026-06-10 20:58:27 +0530

    Time: 3 ms (34.82%) | Memory: 76.4 MB (99.28%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,31 +11,82 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        int carr = 0;
+        ListNode* prev = nullptr;
 
-        ListNode* result=new ListNode(0);
-        ListNode* ptr=result;
-        int carry=0;
-        while(l1!=nullptr || l2!=nullptr){
-            int sum=0+carry;
-            if(l1!=nullptr){
-                sum+=l1->val;
-                l1=l1->next;
-            }
-            if(l2!=nullptr){
-                sum+=l2->val;
-                l2=l2->next;
-            }
 
-            carry=sum/10;
-            sum=sum%10;
-            ptr->next=new ListNode(sum);
-            ptr=ptr->next;
+        while(temp1!=nullptr && temp2!=nullptr){
+            int d1 = temp1->val;
+            int d2 = temp2->val;
+            int sum = d1+d2+carr;
+            if(sum>9){
+                temp1->val=sum-10;
+                carr = 1;
+            }else{
+                temp1->val=sum;
+                carr=0;
+            }
+            prev= temp1;
+            temp1=temp1->next;
+            temp2=temp2->next;
         }
-        if(carry==1) ptr->next= new ListNode(1);
+        while(temp1!=nullptr){
+            int d1 = temp1->val;
+            int sum = d1+carr;
+            if(sum>9){
+                temp1->val=sum-10;
+                carr = 1;
+            }else{
+                temp1->val=sum;
+                carr=0;
+            }
+            prev=temp1;
+            temp1=temp1->next;
+        }
+        
+        temp1=temp2;
+        prev->next = temp1;
 
-        return result->next;
+        while(temp1!=nullptr){
+            int d1 = temp1->val;
+            int sum = d1+carr;
+            if(sum>9){
+                temp1->val=sum-10;
+                carr = 1;
+            }else{
+                temp1->val=sum;
+                carr=0;
+            }
+            prev=temp1;
+            temp1=temp1->next;
+        }
 
 
+        // while(temp2!=nullptr){
+        //     int d2 = temp2->val;
+        //     int sum = d2+carr;
+        //     if(sum>9){
+        //         temp1->next=new ListNode(sum-10);
+        //         carr = 1;
+        //     }else{
+        //         temp1->next=new ListNode(sum);
+        //         carr=0;
+        //     }
+        //     prev=temp1;
+        //     temp1=temp1->next;
+        //     temp2=temp2->next;
+        // }
+
+
+        if(carr){
+            prev->next = new ListNode(1);
+        }
+        
+        
+
+        return l1;
 
     }
 };
