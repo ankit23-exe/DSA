f231cacaf8036e479719cb9bef5c705335894f72
Ankit <23it034@jssaten.ac.in>
2026-07-26 16:02:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (35.52%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -2,9 +2,13 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         if(n==0) return {0};
-        vector<int>ans;
-        for(int i=0;i<=n;i++){
-            ans.push_back(__builtin_popcount(i));
+        vector<int>ans(n+1);
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){ //odd number
+                ans[i]=ans[i-1]+1;
+            }else{
+                ans[i]=ans[i/2];
+            }
         }
         return ans;
     }
