dddc223a226b4797c16ce9dfb681355c3fb0bfdb
Ankit <23it034@jssaten.ac.in>
2026-07-29 13:36:29 +0530

    Time: 86 ms (46.04%) | Memory: 182.1 MB (38.41%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,48 +1,42 @@
 class LRUCache {
 public:
-    int n;
-    list<int> dll;   //key store krenga , linked list mai 
-    unordered_map<int,pair< list<int>::iterator ,int>> mp; //key -> addres,val
+    list<int> dll;
+   unordered_map<int, pair<list<int>::iterator, int>> mp;
+    int siz;
     LRUCache(int capacity) {
-        n=capacity;
-
-        
-    }
-
-    void makeRecentlyUsed(int key){
-        auto ads=mp[key].first;
-        dll.erase(ads);
-        dll.push_front(key);
-        mp[key].first=dll.begin();
+        siz = capacity;
     }
     
     int get(int key) {
-
-        if(mp.find(key)==mp.end()){
-            return -1;
+        if(!mp.count(key)) return -1;
+        else{
+            auto p = mp[key];
+            dll.erase(p.first);
+            dll.push_front(key);
+            mp[key]={dll.begin(),p.second};
+            return p.second;
         }
-        makeRecentlyUsed(key);
-        return mp[key].second;
-        
     }
     
     void put(int key, int value) {
-        if(mp.find(key)!=mp.end()){  //pehla sa present hai
-            makeRecentlyUsed(key);
-            mp[key].second=value;
-        }else{                    //agar nhi present toh
+         if(mp.count(key)){ //present then update it
+            auto p = mp[key];
+            dll.erase(p.first);
             dll.push_front(key);
             mp[key]={dll.begin(),value};
-            n--;
-        }
-
-        if(n<0){
-            int last_key=dll.back();
-            dll.erase(mp[last_key].first);
-            mp.erase(last_key);
-            n++;
-        }
-        
+            return;
+         }else{
+            
+                dll.push_front(key);
+                mp[key]={dll.begin(),value};
+                if(dll.size()>siz){
+                    int lastKey = dll.back();
+                    dll.pop_back();
+                    mp.erase(lastKey);
+                   
+                }
+            
+         }
     }
 };
 
