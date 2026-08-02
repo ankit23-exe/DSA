7d1a91024fcbd62079865ce7927bdb702d855a87
Ankit <23it034@jssaten.ac.in>
2026-08-02 23:30:55 +0530

    Time: 202 ms (10.39%) | Memory: 30.8 MB (49.06%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     int t[201][20001];
-    bool solve(int i, vector<int>& nums,int target){
-        if(target<0) return false;
-        if(target==0) return true;
-        if(i>=nums.size())return false;
+    bool solve(int i, vector<int>& nums, int target) {
 
-        if(target>=0 && t[i][target]!=-1) return t[i][target];
-
-        
-        if(solve(i+1,nums,target-nums[i])||
-        solve(i+1,nums,target)){
-            if(target>=0) return t[i][target]=true;
+        if (target == 0)
             return true;
+        if (i >= nums.size())
+            return false;
 
-        } 
+        if (t[i][target] != -1)
+            return t[i][target];
 
-        if(target>=0) return t[i][target]=false;
-        return false;
+        bool take = false;
+        if (nums[i] <= target)
+            take = solve(i + 1, nums, target - nums[i]);
 
+        bool skip = solve(i + 1, nums, target);
+
+        return t[i][target] = take || skip;
     }
     bool canPartition(vector<int>& nums) {
-        memset(t,-1,sizeof(t));
-        int totalsum = accumulate(nums.begin(),nums.end(),0);
-        if(totalsum%2!=0) return false;
-        int target = totalsum/2;
-        return solve(0,nums,target);
-        
-        
+        memset(t, -1, sizeof(t));
+        int totalsum = accumulate(nums.begin(), nums.end(), 0);
+        if (totalsum % 2 != 0)
+            return false;
+        int target = totalsum / 2;
+        return solve(0, nums, target);
     }
 };
