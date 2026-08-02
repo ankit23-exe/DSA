92b45dafc2a3716516d8a4a2b513284de46f9af2
Ankit <23it034@jssaten.ac.in>
2026-08-02 13:53:45 +0530

    Time: 11 ms (12.56%) | Memory: 14.3 MB (20.73%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
     int n;
-    bool checkConnection(unordered_map<int,vector<int>>&adj,int u,int v){        //true mtlb connection hai
-        vector<bool>vis(n,false);
-        queue<int>que;
-        que.push(u);
-        while(!que.empty()){
-            int f = que.front();
-            que.pop();
-            if(vis[f]==true) continue;
-            vis[f]=true;
-            for(int neigh:adj[f]){
-                if(neigh==v) return true;
-                if(vis[neigh]==false){
-                    que.push(neigh);
-                }
-            }
+    bool checkConnection(unordered_map<int, vector<int>>& adj,
+                         vector<bool>& vis, int u,
+                         int v) { // true mtlb connection hai
 
+        if (u == v)
+            return true;
+
+        // if (vis[u] == true)
+        //     return false;
+        vis[u] = true;
+        for (int& neigh : adj[u]) {
+
+            if (vis[neigh] == false) {
+
+                if (checkConnection(adj, vis, neigh, v))
+                    return true;
+            }
         }
+
         return false;
     }
+
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         n = edges.size();
-        unordered_map<int,vector<int>>adj;
-        for(auto &e:edges){
+        vector<bool> vis(n+1, false);
+        unordered_map<int, vector<int>> adj;
+        for (auto& e : edges) {
             int u = e[0];
-            int v=e[1];
-            if(adj.count(u) && adj.count(v) && checkConnection(adj,u,v)){
+            int v = e[1];
+            if (adj.count(u) && adj.count(v) &&
+                checkConnection(adj, vis, u, v)) {
                 return e;
             }
+            fill(vis.begin(), vis.end(), false);
+           
             adj[u].push_back(v);
             adj[v].push_back(u);
-
         }
         return {};
     }
