4860904d280eb3b93c7fff1ca99b9fa33f8c0b78
Ankit <23it034@jssaten.ac.in>
2026-08-03 09:50:46 +0530

    Time: 7 ms (17.64%) | Memory: 13.4 MB (44.51%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
-    int n;
-    bool checkConnection(unordered_map<int, vector<int>>& adj,
-                         vector<bool>& vis, int u,
-                         int v) { // true mtlb connection hai
-
-        if (u == v)
-            return true;
-
-        // if (vis[u] == true)
-        //     return false;
-        vis[u] = true;
-        for (int& neigh : adj[u]) {
-
-            if (vis[neigh] == false) {
-
-                if (checkConnection(adj, vis, neigh, v))
-                    return true;
-            }
-        }
-
-        return false;
+    int find(int i,vector<int>&parent){
+        if(i==parent[i]) return i;
+        return parent[i]=find(parent[i],parent);
     }
-
+    void Union(int x,int y,vector<int>&parent,vector<int>&rank){
+        int x_parent = find(x,parent);
+        int y_parent = find(y,parent);
+        if(x_parent == y_parent) return;
+        if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent]=x_parent;
+        }else if(rank[x_parent]<rank[y_parent]){
+            parent[x_parent]=y_parent;
+        }else{
+            parent[y_parent]=x_parent;
+            rank[x_parent]+=1;
+        }
+    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        n = edges.size();
-        vector<bool> vis(n+1, false);
-        unordered_map<int, vector<int>> adj;
-        for (auto& e : edges) {
+        int n = edges.size();
+        vector<int>parent(n+1,0);
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+        }
+        vector<int>rank(n+1,0);
+
+        for(auto e:edges){
             int u = e[0];
             int v = e[1];
-            if (adj.count(u) && adj.count(v) &&
-                checkConnection(adj, vis, u, v)) {
+            int pu = find(u,parent);
+            int pv = find(v,parent);
+
+            if(pu==pv){
                 return e;
             }
-            fill(vis.begin(), vis.end(), false);
-           
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            Union(u,v,parent,rank);
         }
         return {};
+
+
+        
     }
 };
